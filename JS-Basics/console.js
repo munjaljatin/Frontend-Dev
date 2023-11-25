37c772c91d6b463df2831ea6c05b75ba70cd2386
Jatin Munjal <munjal.jatin24@gmail.com>
2023-11-25 14:08:01 +0530

    Addes JS Basics Codes

--- JS-Basics/console.js
+++ JS-Basics/console.js
@@ -3,3 +3,9 @@ const fName = "Jatin";
 const lName = "Munjal";
 
 console.log("Welcome!", fName, lName);
+
+let x = console.log("Jatin"); // Jatin is the input value given to the function
+// the function actually prints the given input value on the screen
+// then returns the value undefined, no matter what the input was
+console.log(x);
+console.log(typeof x);
