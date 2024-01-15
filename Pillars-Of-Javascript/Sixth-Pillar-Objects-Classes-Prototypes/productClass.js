956c32212ffa0a320c133e778b7b882cdf7b6c89
Jatin Munjal <munjal.jatin24@gmail.com>
2024-01-15 16:31:02 +0530

    Added OOPS Section

---
+++ Pillars-Of-Javascript/Sixth-Pillar-Objects-Classes-Prototypes/productClass.js
@@ -0,0 +1,18 @@
+// Author: Jatin Munjal
+// Product Class
+
+class Product {
+  // Data Members
+  // no need of let, const and var
+  name;
+  price;
+  discount;
+  description;
+
+  //   behaviours
+  //   no need of function keyword for creating member functions
+  buy() {}
+}
+
+// syntax for creating an object in javascript
+const product1 = new Product();
